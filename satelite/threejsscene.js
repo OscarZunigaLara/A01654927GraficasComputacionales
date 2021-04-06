1489e13aaa12c9c098fbea57caa07dfc45083b5a
Oscar Zuniga <a01654827@itesm.mx>
2021-04-05 20:49:32 -0700

    avance

--- satelite/threejsscene.js
+++ satelite/threejsscene.js
@@ -18,6 +18,7 @@ let material = new THREE.MeshPhongMaterial({ map: texture });
 
 
 let group = new THREE.Object3D;
+let figureList = []
 
 scene = new THREE.Scene();
 
@@ -25,15 +26,61 @@ scene = new THREE.Scene();
 function addElement(){
     console.log("AddElement")
 
-    createCube();
-}
+    figure = random(0,3)
 
+    switch (figure) {
+        case 0:
+            createSphere()
+            //alert('case 1 executed');
+            break;
+        case 1:
+            createCube()
+            //alert("case 2 executed");
+            break;
+        case 2: 
+            createCylinder()
+            break;
+        case 3:
+            createDodecahedron()
+            break;
+            
+    //console.log(figure)
+    
+
+    //createCube();
+    }
+}
 
 function addSatellite(){
     console.log("Satelite")
+    figure = random(0,3)
+
+    //console.log(figure)
+    //console.log(figureList[figureList.length - 1])
+    //createSphereChild(figureList[figureList.length - 1])
+
+
+    switch (figure) {
+        case 0:
+            createSphereChild(figureList[figureList.length - 1])
+            //alert('case 1 executed');
+            break;
+        case 1:
+            createDodecahedronChild(figureList[figureList.length - 1])
+            //alert("case 2 executed");
+            break;
+        case 2: 
+            createCylinderChild(figureList[figureList.length - 1])
+        break;
+        case 3:
+            createCubeChild(figureList[figureList.length - 1])
+            break;
+            
+    //console.log(figure)
     
 
-    console.log(group)
+    //createCube();
+    }
     //createCube();
 }
 
@@ -43,6 +90,23 @@ function reset(){
 
     //scene.remove(cubeGroup);
     //let cubeGroup = new THREE.Object3D;
+    //createScene(canvas)
+    console.log(scene.children)
+    scene.children.splice('Object3D')
+
+    //for (i in scene.children){
+        //if ((scene.children[i].type) == 'Object3D'){
+            //console.log(scene.children[i].type)        
+        //}
+    //}
+    let light = new THREE.DirectionalLight( 0xffffff, 1.0);
+
+    // Position the light out from the scene, pointing at the origin
+    light.position.set(-.5, .2, 1);
+    light.target.position.set(0,-2,0);
+    scene.add(light);
+    figureList = [];
+
 }
 
 
@@ -57,6 +121,10 @@ function main() {
     run();
 }
 
+function random(min, max) {
+    return ~~(Math.random() * (max - min + 1) + min);
+  }
+
 function animate() 
 {
 
@@ -67,34 +135,14 @@ function animate()
     let angle = Math.PI * 2 * fract;
 
     // Rotate the cube about its Y axis
-    cube.rotation.y += angle;
+    //cube.rotation.y += angle;
+    for (i in figureList){
+       //console.log(figureList[i])
+       figureList[i].rotation.y += angle;
+    }
 
     // Rotate the sphere group about its Y axis
 }
-
-function createCube(){
-    console.log("Creating cube")
-    let geometry = new THREE.BoxGeometry(1, 1, 1);
-    cube = new THREE.Mesh(geometry, material);
-
-    cube.rotation.x = Math.PI / 5;
-    cube.rotation.y = Math.PI / 5;
-
-    // Add the cube mesh to our group
-    group.add( cube );
-    group.position.set(1, 0, -0.5);
-    console.log("cube position:" , cube);
-    // Create a group for the sphere
-    scene.add( group );
-    // add mouse handling so we can rotate the scene
-    addMouseHandler(canvas, group);
-    // This code gets the world position of the cone.
-    group.updateMatrixWorld();
-    
-    
-}
-
-
 function run() {
     requestAnimationFrame(function() { run(); });
     
@@ -102,7 +150,270 @@ function run() {
     renderer.render( scene, camera );
 
     // Spin the cube for next frame
-    //animate();
+    animate();
+}
+
+function createSphereChild(father){
+    console.log("Creating Sphere")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.SphereGeometry(.7, 32, 32);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    
+    father.add(newGroup)
+    
+    //scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    //addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    //newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+    
+
+}
+
+function createSphere(){
+    console.log("Creating Sphere")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.SphereGeometry(.7, 32, 32);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+    
+
+}
+
+function createDodecahedron(){
+    console.log("Creating Dodecahedron")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.DodecahedronGeometry(.7);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+
+}
+function createDodecahedronChild(father){
+    console.log("Creating Dodecahedron")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.DodecahedronGeometry(.7);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    father.add(newGroup)
+
+    //scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    //addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    //newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+
+}
+
+function createCylinder(){
+    console.log("Creating Cylinder")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.CylinderGeometry(.5, .5, .5, 32);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+}
+
+function createCylinderChild(father){
+    console.log("Creating Cylinder")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.CylinderGeometry(.5, .5, .5, 32);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    father.add( newGroup )
+    
+    //scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    //addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    //newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+}
+
+function createCube(){
+    console.log("Creating cube")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+    
+}
+function createCubeChild(father){
+    console.log("Creating cube")
+    x = random(-10, 10) / 5
+    y = random(-10, 10) / 5
+    z = random(-10, 10) / 5
+
+    //console.log(x, y, z)
+    let newGroup = new THREE.Object3D;
+
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let cube1 = new THREE.Mesh(geometry, material);
+    figureList.push(cube1);
+    cube1.rotation.x = Math.PI / 5;
+    cube1.rotation.y = Math.PI / 5;
+
+    // Add the cube mesh to our group
+    newGroup.add( cube1 );
+    newGroup.position.set(x, y, z);
+    
+    father.add(newGroup);
+    //scene.add( newGroup );
+
+    // add mouse handling so we can rotate the scene
+    //addMouseHandler(canvas, newGroup);
+    // This code gets the world position of the cone.
+    //newGroup.updateMatrixWorld();
+    //console.log(scene)
+    //console.log("cube position:" , cube);
+    // Create a group for the sphere
+    //console.log(cube1)
+    //console.log(group)
+    
 }
 
 
@@ -141,21 +452,8 @@ function createScene(canvas)
     let ambientLight = new THREE.AmbientLight(0xffccaa, 0.2);
     scene.add(ambientLight);
 
-
     // Create the cube geometry
-    let geometry = new THREE.BoxGeometry(2, 2, 2);
-
-    // And put the geometry and material together into a mesh
-    cube = new THREE.Mesh(geometry, material);
-
-    // Tilt the mesh toward the viewer
-    cube.rotation.x = Math.PI / 5;
-    cube.rotation.y = Math.PI / 5;
-
-    // Add the cube mesh to our group
-    group.add( cube );
-    group.position.set(1, 0, -0.5);
-    console.log("cube position:" , cube);
+    
     // Create a group for the sphere
     scene.add( group );
     // add mouse handling so we can rotate the scene
